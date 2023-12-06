0a5b3aaac509d4f1c17a5455cb7ad67f97079b4d
shaomq <liuyang@galasports.com>
2023-12-06 12:27:37 +0800

    update install

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 pip install torch==1.13.1+cu116 torchvision --index-url https://download.pytorch.org/whl/cu116
-pip install git+https://github.com/NVlabs/tiny-cuda-nn/#subdirectory=bindings/torch
-pip install hydra-core
-pip install chumpy numpy==1.22.4 scipy==1.7.0
-pip install pytorch-lightning
+pip install hydra-core==1.3.2
+pip install pytorch-lightning==2.1.2
 pip install imageio
 pip install ./submodules/diff-gaussian-rasterization
 pip install ./submodules/simple-knn
+pip install git+https://github.com/NVlabs/tiny-cuda-nn/#subdirectory=bindings/torch
